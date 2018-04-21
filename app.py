5892aa5f0f1c9e0a1037b1b2a51b6ba41c14e9a3
Sharon <sharonjin17@gmail.com>
2018-04-20 21:18:35 -0400

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,9 @@ app = Flask(__name__)
 def main():
     return render_template('index.html')
 
+@app.route('/pokemon/<query>')
+def pokemon(query):
+    return '<h1>You are on the ' + query + ' page.</h1>'
 
 if __name__ == '__main__':
     app.run()
