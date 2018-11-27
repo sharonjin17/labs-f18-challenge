1c324177c551ed2d469bc337d19e02bf2a86715d
Sharon <sharonjin17@gmail.com>
2018-11-26 23:28:55 -0500

    Update app.py
    Fully working

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
 from flask import Flask, render_template
+import requests
 
 app = Flask(__name__)
 
+pokemon_data = {}
+name = ""
+number = 1
+
+def is_int(num):
+    try:
+        int(num)
+        return True
+    except ValueError:
+        return False
 
 @app.route('/', methods=['GET'])
 def main():
@@ -9,7 +20,14 @@ def main():
 
 @app.route('/pokemon/<query>')
 def pokemon(query):
-    return '<h1>You are on the ' + query + ' page.</h1>'
+    url = "https://www.pokeapi.co/api/v2/pokemon/" + query
+    pokemon_data = requests.get(url).json()
+    name = pokemon_data.get("forms")[0].get("name")
+    number = pokemon_data.get("game_indices")[0].get("game_index")
+    if is_int(query):
+        return '<h1>The pokemon with id ' + str(number) + ' is ' + name + ' </h1>'
+    else:
+        return '<h1>' + name + ' has id ' + str(number) + '</h1>'
 
 if __name__ == '__main__':
     app.run()
